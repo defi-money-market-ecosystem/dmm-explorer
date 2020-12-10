f715aa974f1c8b5a825d7ea688e171e07042ddec
aknuck <adamknuckey@gmail.com>
2020-12-10 01:03:50 -0500

    updated to match updated data from server

--- src/components/AssetIntroducers/AssetIntroducers.js
+++ src/components/AssetIntroducers/AssetIntroducers.js
@@ -28,32 +28,24 @@ class AssetIntroducers extends React.Component {
         let constructedCountryData = [];
 
         for (let country in countries) {
-          const country_name = countries[country]["AFFILIATE"] ? countries[country]["AFFILIATE"] && countries[country]["AFFILIATE"][0]["country_name"] : countries[country]["PRINCIPAL"] && countries[country]["PRINCIPAL"][0]["country_name"];
-          const total_affiliates = countries[country]["AFFILIATE"] && countries[country]["AFFILIATE"].length;
-          const total_principals = countries[country]["PRINCIPAL"] && countries[country]["PRINCIPAL"].length;
-          const available_affiliates = countries[country]["AFFILIATE"] && countries[country]["AFFILIATE"].filter(country => country["is_primary_market"]).length;
-          const available_principals = countries[country]["PRINCIPAL"] && countries[country]["PRINCIPAL"].filter(country => country["is_primary_market"]).length;
+
+          const country_name = countries[country]['country_name'];
+          const available_affiliates = countries[country]['affiliates'].filter(affiliate => !affiliate['is_on_secondary_market']).length;
+          const available_principals = countries[country]['principals'].filter(affiliate => !affiliate['is_on_secondary_market']).length;
           const total_available = (available_affiliates || 0) + (available_principals || 0);
-          const total_purchased = ((total_affiliates || 0) + (total_principals || 0)) - total_available;
-          const price_usd = countries[country]["AFFILIATE"] ? countries[country]["AFFILIATE"] && countries[country]["AFFILIATE"][0]["price_usd"] : countries[country]["PRINCIPAL"] && countries[country]["PRINCIPAL"][0]["price_usd"];
-          const price_dmg = countries[country]["AFFILIATE"] ? countries[country]["AFFILIATE"] && countries[country]["AFFILIATE"][0]["price_dmg"] : countries[country]["PRINCIPAL"] && countries[country]["PRINCIPAL"][0]["price_dmg"];
-          const affiliate_token_id = countries[country]["AFFILIATE"] && countries[country]["AFFILIATE"][0]["token_id"];
-          const principal_token_id = countries[country]["PRINCIPAL"] && countries[country]["PRINCIPAL"][0]["token_id"];
+          const purchased_affiliates = countries[country]['affiliates'].filter(affiliate => affiliate['is_on_secondary_market']).length;
+          const purchased_principals = countries[country]['principals'].filter(affiliate => affiliate['is_on_secondary_market']).length;
+          const total_purchased = (purchased_affiliates || 0) + (purchased_principals || 0);
 
           constructedCountryData.push({
             country: country_name,
-            totalAffiliates: total_affiliates,
-            totalPrincipals: total_principals,
             availableAffiliates: available_affiliates,
             availablePrincipals: available_principals,
-            totalPurchased: total_purchased,
             totalAvailable: total_available,
-            priceUSD: price_usd,
-            priceDMG: price_dmg,
-            affiliateTokenID: affiliate_token_id,
-            principalTokenID: principal_token_id
-
-          });
+            purchasedAffiliates: purchased_affiliates,
+            purchasedPrincipals: purchased_principals,
+            totalPurchased: total_purchased,
+          })
         }
 
         let mapData = [['Country', 'Purchased NFTs', 'Available NFTs']];
